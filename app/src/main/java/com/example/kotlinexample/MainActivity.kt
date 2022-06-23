574457b497bb3da4d1130b62ee76421598a43baa
abhishek raghav <abhishek.raghav00@gmail.com>
2022-06-23 14:10:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlinexample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var edt_username = findViewById(R.id.edt_username) as EditText
+//        var edt_password = findViewById(R.id.edt_password) as EditText
+//        var btn_login = findViewById(R.id.btn_login) as Button
+
+    }
+}
